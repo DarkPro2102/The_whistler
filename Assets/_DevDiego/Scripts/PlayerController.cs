d371dae8333bafc38bb988aa0a049100330bd79e
Diego Bejarano <diego_alejandro2000@hotmail.com>
2024-03-30 00:53:49 -0500

    Navmesh añadido en carpeta de prueba

---
+++ Assets/_DevDiego/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                navMeshAgent.destination = hit.point;
+            }
+        }
+    }
+}
